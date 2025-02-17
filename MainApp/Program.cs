8a5d6fc96cfa56320e019e0a2fb54d882ac295d7
Stefan Sjöberg <bird.nerd@outlook.com>
2025-02-17 15:39:15 +0100

    Add project files.

---
+++ MainApp/Program.cs
@@ -0,0 +1,20 @@
+﻿Random topSecret = new();
+int secretNumber = topSecret.Next(1, 101);
+int guessCount = 0;
+
+while (true)
+{
+    guessCount++;
+    Console.WriteLine("Skriv en siffra mellan 1 och 100: ");
+    int guess = int.Parse(Console.ReadLine()!);
+    if (guess == secretNumber)
+    {
+        Console.WriteLine($"Du gissa på nummer {secretNumber} och har vunnit ett avgassystem från datsun!!");
+        Console.WriteLine($"Det tog dig {guessCount} försök att gissa rätt.");
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Försök igen!");
+    }
+}
